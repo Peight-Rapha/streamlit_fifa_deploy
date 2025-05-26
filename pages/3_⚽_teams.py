0be47a36a662c823c910a4d065fa035d5a72b91f
Raphael Rodrigues <80860792+Peight-Rapha@users.noreply.github.com>
2025-05-26 08:24:02 -0300

    Update 3_⚽_teams.py

--- pages/3_⚽_teams.py
+++ pages/3_⚽_teams.py
@@ -12,41 +12,39 @@ df_data = st.session_state['data']
 # Criando seletor de clubes
 clubes = df_data['Club'].unique()
 club = st.sidebar.selectbox("Club", clubes)
-# Montar novo dataframe com base no que foi filtrado no clube
-df_filtered = df_data[df_data['Club'] == club].set_index('Name')
 
+# Filtrar pelo clube selecionado
+df_filtered = df_data[df_data['Club'] == club]
 
+# Criando seletor de posição com base no clube filtrado
 posicoes = df_filtered['Position'].unique()
 posicao = st.sidebar.selectbox("Position", posicoes)
-st.markdown(f"**Posição:** {df_filtered['Position'].values[0]}")
-# filtrando pela posição
-df_filtered = df_filtered[df_filtered['Position'] == df_filtered['Position'].values[0]]
 
-# Criando selector de imagem do club
-st.image(df_filtered.iloc[0]["Club Logo"])
-# Criando subtítul com o nome do clube
-st.markdown(F"## {club}")
+# Filtrar pelo posição selecionada
+df_filtered = df_filtered[df_filtered['Position'] == posicao].set_index('Name')
 
-# Lista nome das colunas do df_filtered
-columns = df_filtered.columns.to_list()
+# Exibir informações do clube e posição
+if not df_filtered.empty:
+    st.image(df_filtered.iloc[0]["Club Logo"])
+    st.markdown(f"## {club}")
+    st.markdown(f"**Posição:** {posicao}")
 
-# Selecionando colunas desejadas
-columns = ["Age", "Photo", "Flag", "Overall", "Value(£)", "Wage(£)", "Joined", 
-           "Height(cm.)", "Weight(lbs.)", 
-           "Contract Valid Until", "Release Clause(£)"]
+    # Selecionando colunas desejadas
+    columns = ["Age", "Photo", "Flag", "Overall", "Value(£)", "Wage(£)", "Joined", 
+               "Height(cm.)", "Weight(lbs.)", 
+               "Contract Valid Until", "Release Clause(£)"]
 
-# Criar um dataframe usando streamlit para permitir personalização
-st.dataframe(df_filtered[columns],
-             column_config={
-                 "Overall": st.column_config.ProgressColumn(
-                     "Overall", format= "%d", min_value=0, max_value=100
-                 ),
-                 # Personalizar coluna "Wage(£)" por renumeração mais alta
-                 "Wage(£)": st.column_config.ProgressColumn(
-                     "Weekly Wage", format = "£%d", min_value=0, max_value=df_filtered["Wage(£)"].max()
-                 ),
-                 # Configurar "Photo" como imagem
-                 "Photo": st.column_config.ImageColumn(),
-                 # Bandeira do clube
-                 "Flag": st.column_config.ImageColumn("Country")
-            })
+    # Criar um dataframe usando streamlit para permitir personalização
+    st.dataframe(df_filtered[columns],
+                 column_config={
+                     "Overall": st.column_config.ProgressColumn(
+                         "Overall", format= "%d", min_value=0, max_value=100
+                     ),
+                     "Wage(£)": st.column_config.ProgressColumn(
+                         "Weekly Wage", format = "£%d", min_value=0, max_value=df_filtered["Wage(£)"].max()
+                     ),
+                     "Photo": st.column_config.ImageColumn(),
+                     "Flag": st.column_config.ImageColumn("Country")
+                })
+else:
+    st.warning("Nenhum jogador encontrado para esse clube e posição.")
